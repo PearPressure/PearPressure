d6127129b4e4ccf58f4246aafb00f920cff6711f
jlunder00 <jasonlunder@comcast.net>
2020-05-16 01:35:10 -0700

    Working Call Function

--- src/createDemoRoom.js
+++ src/createDemoRoom.js
@@ -1,6 +1,21 @@
-const newRoomEndpoint = 'https://fu6720epic.execute-api.us-west-2.amazonaws.com/default/dailyWwwApiDemoNewCall',
-    tokenEndpoint = 'https://dwdd5s2bp7.execute-api.us-west-2.amazonaws.com/default/dailyWWWApiDemoToken';
+const newRoomEndpoint = 'https://pearpressure.daily.co/test-call/meeting-tokens/'
+tokenEndpoint = 'https://pearpressure.daily.co/test-call/meeting-tokens/token';
+/*var data = "{\"properties\":{\"hide_daily_branding\":false}}";
 
+var xhr = new XMLHttpRequest();
+
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("POST", "https://api.daily.co/v1/");
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("authorization", "Bearer 44b40a95595ee9a1b106706e6e4dede27b4d0a06eb091eedd2d5240ee722a263");
+
+xhr.send(data);
+*/
 async function createMtgRoom() {
     try {
         let response = await fetch(newRoomEndpoint),
@@ -15,13 +30,17 @@ async function createMtgLinkWithToken(room, properties = {}) {
     try {
         let response = await fetch(
             tokenEndpoint, {
-            method: 'POST',
-            body: JSON.stringify({
-                properties: {
-                    room_name: room.name, ...properties
-                }
-            })
-        },
+                method: 'POST',
+                url: 'https://pearpressure.daily.co/test-call/meeting-tokens',
+                headers: {
+                    authorization: 'Bearer 44b40a95595ee9a1b106706e6e4dede27b4d0a06eb091eedd2d5240ee722a263'
+                },
+                body: JSON.stringify({
+                    properties: {
+                        room_name: room.name, ...properties
+                    }
+                })
+            },
         );
         let token = await response.text();
         return `${room.url}?t=${token}`;
@@ -29,3 +48,4 @@ async function createMtgLinkWithToken(room, properties = {}) {
         console.error(e);
     }
 }
+
