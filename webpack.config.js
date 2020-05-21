81696db8dc3f551e63ebf6bd6da374d690f193ed
jlunder00 <jasonlunder@comcast.net>
2020-05-21 13:17:54 -0700

    reorganized into folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
+const path = require('path');
+
 module.exports = {
     //mode: 'development',
-    entry: './renderer.js',
+    entry: './src/renderer.js',
     output: {
-        filename: './renderer-bundle.js'
+        filename: 'renderer-bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
             {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader',
+                // loads .html files
+                test: /\.(html)$/,
+                include: [path.resolve(__dirname, "src")],
+                use: {
+                    loader: "html-loader",
+                },
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+                include: [path.resolve(__dirname, "src")],
+                use: "url-loader",
             },
             {
                 test: /\.css$/i,
+                include: [path.resolve(__dirname, "src")],
                 use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.js$/,
+                include: [path.resolve(__dirname, "src")],
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
@@ -23,7 +37,9 @@ module.exports = {
                         presets: ['@babel/env', '@babel/react'],
                     },
                 },
-
+                resolve: {
+                    extensions: [".js", ".jsx", ".json"],
+                },
             },
         ],
     },
