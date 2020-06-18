246f9ba4ae54f2b21d0476ef75fb7bfac404f6ab
jlunder00 <jasonlunder@comcast.net>
2020-06-18 13:24:40 -0700

    working room deletion

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -5,8 +5,8 @@ import VideoCallFrame from 'Pages/VideoCallFrame.js'
 import DailyIframe from "@daily-co/daily-js"
 
 
-let roomName = "https://pearpressure.daily.co/DRvskFeilid51C5hEf2C";
-
+const auth = 'Bearer 44b40a95595ee9a1b106706e6e4dede27b4d0a06eb091eedd2d5240ee722a263';
+let roomName = '';
 
 function Button() {
     const [count, setCount] = useState(0);
@@ -35,17 +35,42 @@ function Button() {
             method: "GET",
             url: 'https://api.daily.co/v1/rooms',
             headers: {
-                authorization: 'Bearer 44b40a95595ee9a1b106706e6e4dede27b4d0a06eb091eedd2d5240ee722a263'
+                authorization: auth
             }
         };
-        request(options, function (error, response, body) {
+        request(options, callback);
+        function callback(error, response, body) {
             if (error) throw new Error(error);
-            console.log(body);
+            //console.log(body);
             var bodyParsed = JSON.parse(body);
             console.log(bodyParsed);
-            roomName = bodyParsed.data[0].url;
-        });
+            roomName = bodyParsed.data[0].name;
+        }
         console.log(roomName);
+        /*console.log(getRequest);
+        console.log(getRequest.host);
+        console.log(getRequest.response);
+        console.log(getRequest.body);
+        console.log(getRequest.agent);*/
+        //var data = JSON.parse(getRequest.response.body.data);
+        var theURL = '';
+        //console.log(data);
+        for (var i = 0; i < 1; i++)
+        {
+            theURL = 'https://api.daily.co/v1/rooms/'+roomName;
+            options = {
+                method: "DELETE",
+                url: theURL,
+                headers: {
+                    authorization: auth
+                }
+            };
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+                var theResponse = JSON.parse(body);
+                console.log(theResponse);
+            });
+        }
 
     }
 
@@ -71,7 +96,7 @@ class VideoPage extends React.Component {
                </header>
                 <Button />
                 <VideoCallFrame
-                    url={roomName}
+                    url={'https://pearpressure.daily.co/test-call'}
                 ></VideoCallFrame>
 
             </div>
