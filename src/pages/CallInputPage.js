965776a37ded8c46f3815e38a37ba14f3937a002
jlunder00 <jasonlunder@comcast.net>
2020-07-26 00:52:03 -0700

    trouble in module exports town

--- src/pages/CallInputPage.js
+++ src/pages/CallInputPage.js
@@ -2,7 +2,8 @@ import 'Css/App.css'
 import React, { Component, useState, useEffect } from 'react';
 import { ipcRenderer } from 'electron'
 
-
+const auth = 'Bearer 44b40a95595ee9a1b106706e6e4dede27b4d0a06eb091eedd2d5240ee722a263';
+let roomUrl = '';
 
 function Button() {
     const [count, setCount] = useState(0);
@@ -16,11 +17,60 @@ function Button() {
     }, []);
 
     function myOnClick() {
+        var request = require("request");
         setName(!name);
-        if (nameMonitor == "false")
+        if (nameMonitor == "false") {
+
             setNameMonitor("true");
-        else
+        }
+        else {
             setNameMonitor("false");
+        }
+        var options = {
+            method: "POST",
+            url: 'https://api.daily.co/v1/rooms',
+            headers: {
+                'content-type': 'application/json',
+                authorization: auth
+            }
+        };
+        request(options, callback);
+        function callback(error, response, body) {
+            if (error) throw new Error(error);
+            //console.log(body);
+            var bodyParsed = JSON.parse(body);
+            roomUrl = bodyParsed.url;
+            //roomName = bodyParsed.data[0].name;
+        }
+        /*const roomName = () => {
+            return roomUrl;
+        };
+        console.log(module);
+        module.exports.log = 'hello world';*/
+        //console.log(roomName);
+        /*console.log(getRequest);
+        console.log(getRequest.host);
+        console.log(getRequest.response);
+        console.log(getRequest.body);
+        console.log(getRequest.agent);*/
+        //var data = JSON.parse(getRequest.response.body.data);
+        /*var theURL = '';
+        //console.log(data);
+        for (var i = 0; i < 1; i++) {
+            theURL = 'https://api.daily.co/v1/rooms/' + roomName;
+            options = {
+                method: "DELETE",
+                url: theURL,
+                headers: {
+                    authorization: auth
+                }
+            };
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+                var theResponse = JSON.parse(body);
+                console.log(theResponse);
+            });
+        }*/
         ipcRenderer.send('asynchronous open Video', ('open call window', 'VideoPage'))
     }
 
--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -4,9 +4,9 @@ import React, { useState, useEffect } from 'react';
 import VideoCallFrame from 'Pages/VideoCallFrame.js'
 import DailyIframe from "@daily-co/daily-js"
 
-
-const auth = 'Bearer 44b40a95595ee9a1b106706e6e4dede27b4d0a06eb091eedd2d5240ee722a263';
-let roomName = '';
+//const user = require('./CallInputPage.js');
+//console.log(user);
+let url = 'https://pearpressure.daily.co/test-call';
 
 function Button() {
     const [count, setCount] = useState(0);
@@ -21,57 +21,7 @@ function Button() {
 
 
     function myOnClick() {
-        var request = require("request");
-        setName(!name);
-        if (nameMonitor == "false") {
-
-            setNameMonitor("true");
-        }
-        else
-        {
-            setNameMonitor("false");
-        }
-        var options = {
-            method: "GET",
-            url: 'https://api.daily.co/v1/rooms',
-            headers: {
-                authorization: auth
-            }
-        };
-        request(options, callback);
-        function callback(error, response, body) {
-            if (error) throw new Error(error);
-            //console.log(body);
-            var bodyParsed = JSON.parse(body);
-            console.log(bodyParsed);
-            roomName = bodyParsed.data[0].name;
-        }
-        console.log(roomName);
-        /*console.log(getRequest);
-        console.log(getRequest.host);
-        console.log(getRequest.response);
-        console.log(getRequest.body);
-        console.log(getRequest.agent);*/
-        //var data = JSON.parse(getRequest.response.body.data);
-        var theURL = '';
-        //console.log(data);
-        for (var i = 0; i < 1; i++)
-        {
-            theURL = 'https://api.daily.co/v1/rooms/'+roomName;
-            options = {
-                method: "DELETE",
-                url: theURL,
-                headers: {
-                    authorization: auth
-                }
-            };
-            request(options, function (error, response, body) {
-                if (error) throw new Error(error);
-                var theResponse = JSON.parse(body);
-                console.log(theResponse);
-            });
-        }
-
+        
     }
 
 
@@ -93,7 +43,7 @@ class VideoPage extends React.Component {
             <div className="VideoPage">
 
                 <VideoCallFrame
-                    url={'https://pearpressure.daily.co/test-call'}
+                    url={url}
                 ></VideoCallFrame>
 
             </div>
